16dd1b9b44e49a2ebbf3904b9f72d487ece465f0
ocastelblanco <ocastelblanco@gmail.com>
2022-07-06 12:05:18 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mse-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mediateca-se.com';
+}
